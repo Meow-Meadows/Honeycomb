f92fb6b0e0ef5caeb2a952598076727bb76869fc
Leon Mamic <leonmamic111@gmail.com>
2026-06-24 19:57:25 +0200

    Added basic bitboard

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod board;
+use board::Board;
+
 fn main() {
-    println!("Hello, world!");
+    let b = Board::starting_position();
+    println!("honeycomb chess engine :3");
+    println!("white pawns: {:064b}", b.white_pawns);
 }
